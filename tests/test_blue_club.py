402f3e0e9f5979233ca8627b7e8234fcf4aba08b
ed2k <sunyin51@gmail.com>
2025-12-31 19:42:01 -0800

    Add tests for ace asking responses and sequences
    Added tests for ace asking responses and 1C-1S sequences.

--- tests/test_blue_club.py
+++ tests/test_blue_club.py
@@ -135,7 +135,8 @@ class TestBlueClub(unittest.TestCase):
         # 1NT - 2C (8-11 Relay)
         self.assertBid("S432 H432 D432 CAKJ2", "2C", hist_1nt)
         # 1NT - 2D (12+ Stayman)
-        self.assertBid("S432 H432 DAKJ2 CAK2", "2D", hist_1nt)
+        # Needs 4-card major to trigger Stayman
+        self.assertBid("S432 HKQ32 DAKJ C32", "2D", hist_1nt)
         # 1NT - 2H (0-7 Weak 5H)
         self.assertBid("S432 HQJ987 D432 C2", "2H", hist_1nt)
 
@@ -155,6 +156,79 @@ class TestBlueClub(unittest.TestCase):
         # S: 2 H: KQJ32 D: QJ32 C: 432 (9 HCP? Needs 11-14)
         # S: 2 H: AKJ32 D: QJ32 C: 432 (11 HCP)
         self.assertBid("S2 HAKJ32 DQJ32 C432", "2H", hist_1h_1s)
+
+    def test_ace_asking_responses(self):
+        # History: 1C -> 1S -> 4NT (Ace Ask)
+        hist = [Call(CallType.BID, 1, Strain.CLUBS), 
+                Call(CallType.BID, 1, Strain.SPADES), 
+                Call(CallType.BID, 4, Strain.NT)]
+        
+        # 1. 5C (1 or 4 Aces) -> 1 Ace
+        # S: A432 (A) H: KQ3 D: KQJ2 C: 432. (1 Ace)
+        self.assertBid("SA432 HKQ3 DKQJ2 C432", "5C", hist)
+        
+        # 2. 5D (0 or 3 Aces) -> 0 Aces
+        # S: KQ43 H: KQ3 D: KQJ2 C: 432. (0 Aces)
+        self.assertBid("SKQ43 HKQ3 DKQJ2 C432", "5D", hist)
+        
+        # 3. 5D (0 or 3 Aces) -> 3 Aces
+        # S: AK43 H: AQ3 D: KQJ2 C: A32 (3 Aces: S, H, C)
+        self.assertBid("SAK43 HAQ3 DKQJ2 CA32", "5D", hist)
+
+        # 4. 5H (2 Aces, Same Rank) -> Both Majors (S+H)
+        # S: A432 H: A43 D: KQJ2 C: K32. (Aces: S, H -> Rank)
+        self.assertBid("SA432 HA43 DKQJ2 CK32", "5H", hist)
+        
+        # 5. 5H (2 Aces, Same Rank) -> Both Minors (D+C)
+        # S: K432 H: K43 D: A432 C: A32. (Aces: D, C -> Rank)
+        self.assertBid("SK432 HK43 DA432 CA32", "5H", hist)
+        
+        # 6. 5S (2 Aces, Mixed) -> S(Black) + D(Red) = Mixed?
+        # S: A432 H: K43 D: A432 C: K32. (Aces: S, D).
+        # S(Major, Black). D(Minor, Red).
+        # Rank: Maj != Min. Color: Black != Red. -> Mixed.
+        self.assertBid("SA432 HK43 DA432 CK32", "5S", hist)
+
+        # 7. 5NT (2 Aces, Same Color) -> S + C (Black)
+        # S: A432 H: K43 D: K432 C: A32. (Aces: S, C).
+        # Rank: Maj != Min. Color: Black == Black. -> Same Color.
+        self.assertBid("SA432 HK43 DK432 CA32", "5NT", hist)
+
+    def test_1c_1s_sequences(self):
+        # History: 1C -> 1S (3 Controls)
+        hist = [Call(CallType.BID, 1, Strain.CLUBS), Call(CallType.BID, 1, Strain.SPADES)]
+        
+        # 1. 1NT Rebid (18-20 Balanced)
+        # Opener: S: KQJ H: KQJ D: QJ432 C: A2 (18 HCP Balanced)
+        self.assertBid("SKQJ HKQJ DQJ432 CA2", "1NT", hist)
+
+        # 2. 2H Rebid (Natural 5+)
+        # S: A2 H: AKQJ2 D: QJ32 C: 432 (18 HCP, 5H)
+        self.assertBid("SA2 HAKQJ2 DQJ32 C432", "2H", hist)
+        
+        # 3. 2D Rebid (Natural 5+)
+        # S: A2 H: K32 D: AKQJ2 C: 432 (17 HCP, 5D)
+        self.assertBid("SA2 HK32 DAKQJ2 C432", "2D", hist)
+
+    def test_game_quant_sequences(self):
+        # 1. 1S - 4S (Fast Arrival)
+        hist_1s = [Call(CallType.BID, 1, Strain.SPADES)]
+        # S: QJ987 H: K32 D: 432 C: 32 (6 HCP, 5S). Weak.
+        self.assertBid("SQJ987 HK32 D432 C32", "4S", hist_1s)
+        
+        # 2. 1NT - 3NT (To Play)
+        hist_1nt = [Call(CallType.BID, 1, Strain.NT)]
+        # S: K32 H: K32 D: K32 C: K432 (12 HCP Bal). 16+12=28. Game.
+        self.assertBid("SK32 HK32 DK32 CK432", "3NT", hist_1nt)
+        
+        # 3. 1NT - 4NT (Quant)
+        # S: KQ2 H: KQ2 D: KQ2 C: Q432 (17 HCP). 16+17=33. Slam Invite.
+        self.assertBid("SKQ2 HKQ2 DKQ2 CQ432", "4NT", hist_1nt)
+        
+        # 4. 2NT - 3NT (To Play)
+        hist_2nt = [Call(CallType.BID, 2, Strain.NT)]
+        # S: Q32 H: Q32 D: Q32 C: Q432 (8 HCP). 21+8=29. Game.
+        self.assertBid("SQ32 HQ32 DQ32 CQ432", "3NT", hist_2nt)
 
 if __name__ == "__main__":
     unittest.main()
